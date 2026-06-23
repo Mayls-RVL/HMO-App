ffc4fe6c9c0111f5dac0c93c079333fa0ea94a41
jeffrouzel <jrabatog@gmail.com>
2026-06-23 16:36:54 +0800

    Added Color scheme of the app

--- app/src/main/java/com/example/hmo_app/ui/theme/Color.kt
+++ app/src/main/java/com/example/hmo_app/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// 77 THEME
+val DarkRed = Color(0xFF8B1A1A)
+val BrightRed = Color(0xFF900B09)
+val BlackSurface = Color (0xFF000000)
+val WhiteSurface = Color (0xFFFFFFFF)
+val GreySurface = Color(0xFFD9D9D9)
+val LightGreySurface = Color (0xFFE5E7EB)
